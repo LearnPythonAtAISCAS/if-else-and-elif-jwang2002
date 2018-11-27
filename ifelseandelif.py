aa29c01932cb5fe4c05c063f86dc6a8b19f4f332
jwang2002 <38352631+jwang2002@users.noreply.github.com>
2018-11-27 13:09:27 -0500

    Update ifelseandelif.py

--- ifelseandelif.py
+++ ifelseandelif.py
@@ -1,41 +1,16 @@
-### Copyright Adren Poulard
+#Copyright Junbo Wang#
+print("Do you like Fabian?")
+Question3 = input()
 
-## You will now learn how to use if, else & elif statements in Python
+Question3 = (Question3.lower())
 
-##If & else statements
+if Question3 == "yes":
+    print("You should not")
 
-print("Do you like cheese?")
-Question = input()
-
-#Make input all lowercase
-Question = (Question.lower())
-
-#If a, do b. Else, do c.
-if Question == "yes":
-    print("Cool")
-else:
-    print("Oh no")
-
-##Elif statements
-
-print("Do you like bread?")
-Question2 = input()
-
-#Make input all lowercase
-Question2 = (Question2.lower())
-
-#If a, do b. Else if c, do d. Else, do e.
-if Question2 == "yes":
+elif Question3 == "no":
     print("Yay")
 
-elif Question2 == "maybe":
-    print("Oh")
 else:
-    print(":(")
+    print("You don't, right?")
 
-
-##Create your own text-based game/adventure using if, else & elif statements
-##Use at least two if statements, one else statement, and one elif statement
-##Tip: You can have statements within statements!
-
-
+#End#
